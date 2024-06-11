ba77339328c9c36a9d7daa2b0023e8f0b0cbee03
Mohamed2Sherif <91475739+Mohamed2Sherif@users.noreply.github.com>
2024-06-11 14:14:39 +0300

    Problem Determine if Two strigns are close solved using sortign and hashmap

---
+++ MediumProblems/P1657.py
@@ -0,0 +1,57 @@
+"""
+Two strings are considered close if you can attain one from the other using the following operations:
+
+Operation 1: Swap any two existing characters.
+For example, abcde -> aecdb
+Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+You can use the operations on either string as many times as necessary.
+
+Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "bca"
+Output: true
+Explanation: You can attain word2 from word1 in 2 operations.
+Apply Operation 1: "abc" -> "acb"
+Apply Operation 1: "acb" -> "bca"
+Example 2:
+
+Input: word1 = "a", word2 = "aa"
+Output: false
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
+Example 3:
+
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+Explanation: You can attain word2 from word1 in 3 operations.
+Apply Operation 1: "cabbba" -> "caabbb"
+Apply Operation 2: "caabbb" -> "baaccc"
+Apply Operation 2: "baaccc" -> "abbccc"
+"""
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        else:
+            if set(word1) == set(word2):
+                word1_dict = {char:0 for char in word1}
+                word2_dict = {char:0 for char in word2}
+                for char in word1:
+                    if char in word1_dict.keys():
+                        word1_dict[char]+=1
+                for char in word2:
+                    if char in word2_dict.keys():
+                        word2_dict[char]+=1
+
+                test1 = list(sorted(word1_dict.values()))
+                test2 = list(sorted(word2_dict.values()))
+                if test1 == test2 :
+                    return True
+                else:
+                    return False
+                
